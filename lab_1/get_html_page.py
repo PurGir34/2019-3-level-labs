1520b354ffe6138aae5d35c04b7514e704aadf22
Nikita-Korolev <55663240+Nikita-Korolev@users.noreply.github.com>
2019-10-09 21:53:05 +0300

    поправил2

--- lab_1/get_html_page.py
+++ lab_1/get_html_page.py
@@ -5,7 +5,7 @@ from dz import get_html
 class MyTestCase(unittest.TestCase):
     def test_html_page(self):
         url = "https://panorama.pub"
-        a = get_html_page(url)
+        a = get_html(url)
         print(a)
         self.assertEqual(a.status_code, 200)
 
