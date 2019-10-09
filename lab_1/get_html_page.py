d895bff9250c7e57c774e53da166754ee81677f1
PurGir34 <50275986+PurGir34@users.noreply.github.com>
2019-10-09 21:04:32 +0300

    Unit-test

---
+++ lab_1/get_html_page.py
@@ -0,0 +1,14 @@
+import unittest
+from dz import get_html_page
+
+
+class MyTestCase(unittest.TestCase):
+    def test_html_page(self):
+        url = "https://panorama.pub"
+        a = get_html_page(url)
+        print(a)
+        self.assertEqual(a.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
