b05acb15b827d8ca4bc35a2413ff3587e96874f4
Nikita-Korolev <55663240+Nikita-Korolev@users.noreply.github.com>
2019-10-09 21:14:09 +0300

    Добавил второй тест

--- find_articles.py
+++ find_articles.py
@@ -9,6 +9,13 @@ class MyTestCase(unittest.TestCase):
         length_of_dict = len(find_articles(open(url).read())) #gives the total length of the dictionary, this would be equal to the number of items in the dictionary
         self.assertEqual(length_of_dict, 26) #a function to check if two variables are equal
 
+class MyTestCase2(unittest.TestCase):
+
+
+    def test_of_url(self):
+        url = "https://panorama.pub"
+        result = check_url(url)
+        self.assertEqual(result, 200)
 
 if __name__ == '__main__':
     unittest.main()
