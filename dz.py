448bbcf52942160973d220779fd556ad131feeea
rurinaL <shanin.99@mail.ru>
2019-10-23 23:19:24 +0300

    dop

--- dz.py
+++ dz.py
@@ -7,17 +7,31 @@ from flask import render_template
 
 now = datetime.datetime.now()
 date = now.strftime("%d-%m-%Y")
-url = "https://panorama.pub"
-path = "articles.json"
+url1 = "https://panorama.pub"
+url2 = "https://newsland.com"
+url3 = "https://webinnews.ru/"
+path1 = "articles1.json"
+path2 = "articles2.json"
+path3 = "articles3.json"
 fas = "date.json"
 data = {
 
-    "url": url,
+    "url": url1,
     "creationDate": date,
     "articles": ["None"]
 }
+data2 = {
 
+    "url": url2,
+    "creationDate": date,
+    "articles": ["None"]
+}
+data3 = {
 
+    "url": url3,
+    "creationDate": date,
+    "articles": ["None"]
+}
 def get_html(url):
     request = requests.get(url)
     content = request.text
@@ -27,10 +41,24 @@ def get_html(url):
     return (content)
 
 
-def find_articles(lil_html):
+def find_articles1(lil_html1):
     titles = []
-    lil_parsed = BeautifulSoup(lil_html, 'html.parser')
+    lil_parsed = BeautifulSoup(lil_html1, 'html.parser')
     lil_article = lil_parsed.find_all('h3')
+    for title in lil_article:
+        titles.append(title.text.strip())
+    return (titles)
+def find_articles2(lil_html2):
+    titles = []
+    lil_parsed = BeautifulSoup(lil_html2, 'html.parser')
+    lil_article = lil_parsed.find_all('a')
+    for title in lil_article:
+        titles.append(title.text.strip())
+    return (titles)
+def find_articles3(lil_html3):
+    titles = []
+    lil_parsed = BeautifulSoup(lil_html3, 'html.parser')
+    lil_article = lil_parsed.find_all('h2')
     for title in lil_article:
         titles.append(title.text.strip())
     return (titles)
@@ -39,7 +67,7 @@ def find_articles(lil_html):
 def publish_report(path, articles):
     c = []
     for i in articles:
-        d = {"title": i + ' url :https://panorama.pub'}
+        d = {"title": i}
         c.append(d)
     data["articles"] = c
     with open(path, "w", encoding="utf8") as write_file:
@@ -53,18 +81,42 @@ def get_date(date):
 app = Flask(__name__)
 
 
-    
 @app.route('/')
+def start():
+
+    return render_template('news4.html')
+
+
+@app.route('/pano')
 def panorama_articles():
-    lil_html = get_html(url)
+    lil_html = get_html(url1)
     articles = find_articles(lil_html)
-    publish_report(path, articles)
+    publish_report(path1, articles)
     # print(articles)
 
-    with open("articles.json", "r", encoding="utf-8") as read_file1:
+    with open("articles1.json", "r", encoding="utf-8") as read_file1:
         data_set = json.load(read_file1)
-    return render_template('news.html', url=data_set['url'], date=data_set['creationDate'], articles=articles)
+    return render_template('news1.html', url=data_set['url'], date=data_set['creationDate'], articles=articles)
+@app.route('/newsland')
+def newsland_articles():
+    lil_html = get_html(url2)
+    articles = find_articles(lil_html)
+    publish_report(path2, articles)
+    # print(articles)
 
+    with open("articles2.json", "r", encoding="utf-8") as read_file1:
+        data_set = json.load(read_file1)
+    return render_template('news2.html', url=data_set['url'], date=data_set['creationDate'], articles=articles)
+@app.route('/webbi')
+def webbi_articles():
+    lil_html = get_html(url3)
+    articles = find_articles(lil_html)
+    publish_report(path3, articles)
+    # print(articles)
+
+    with open("articles3.json", "r", encoding="utf-8") as read_file1:
+        data_set = json.load(read_file1)
+    return render_template('news3.html', url=data_set['url'], date=data_set['creationDate'], articles=articles)
 
 @app.route('/yfyg')
 def update2():
@@ -73,13 +125,23 @@ def update2():
    
     return render_template('new.html', date=date)
 
-@app.route('/update_page')
-def update_page():
+@app.route('/1ist')
+def redir1():
     return redirect(url_for('panorama_articles'))
+
+@app.route('/2ist')
+def redir2():
+    return redirect(url_for('newsland_articles'))
+
+@app.route('/3ist')
+def redir3():
+    return redirect(url_for('webbi_articles'))
     
+
 @app.route('/task')
 def update2_page():
     return redirect(url_for('update2'))
+
 
 
 if __name__ == "__main__":
